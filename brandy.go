c95c4e6e05d29c7abf6c25b2e89f7570e79d2c9f
David Hazinski <dhazinski@gmail.com>
2015-02-13 07:01:45 -0500

    adding brandy render

--- brandy.go
+++ brandy.go
@@ -12,7 +12,7 @@ func main() {
 	mesh.SmoothNormals()
 	scene.Add(mesh)
 	scene.Add(pt.NewSphere(pt.Vector{-2, 10, -5}, 1, pt.LightMaterial(pt.Color{1, 1, 1}, 12, pt.QuadraticAttenuation(0.1))))
-	scene.Add(pt.NewCube(pt.Vector{-1000, -1, -100}, pt.Vector{100, -0.6, 100}, pt.DiffuseMaterial(pt.Color{1, 1, 1})))
+	scene.Add(pt.NewCube(pt.Vector{-1000, -1, -100}, pt.Vector{100, -0.5, 100}, pt.DiffuseMaterial(pt.Color{1, 1, 1})))
 	camera := pt.LookAt(pt.Vector{0, 1, -3}, pt.Vector{0, 0, 0}, pt.Vector{0, 1, 0}, 45)
-	pt.IterativeRender("renders/brandy%03d.png", 1000, &scene, &camera, 512, 512, -1, 16, 8)
+	pt.IterativeRender("renders/brandy%03d.png", 1000, &scene, &camera, 1024, 1024, -1, 16, 8)
 }
